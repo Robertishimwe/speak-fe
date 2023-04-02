799bd1ff1b25ce0471c25cb5b77eccd9e1fd32dc
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2023-04-02 16:47:22 +0000

    change to parsonalized url

--- script.js
+++ script.js
@@ -164,7 +164,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv);
 
-    const response = await fetch("https://ishimwe.cyclic.app/api/chat/", {
+    const response = await fetch("https://conv.cyclic.app/api/chat", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
